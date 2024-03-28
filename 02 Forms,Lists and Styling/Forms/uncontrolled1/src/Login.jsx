0cfd3266adc7a4d75ffca3a9a8e0ab1fd04af4fb
Francesco <ballerifrancesco@gmail.com>
2024-03-28 11:28:46 +0100

    uncontrolled forms finished

---
+++ 02 Forms,Lists and Styling/Forms/uncontrolled1/src/Login.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react"
+
+
+export function Login({ onLogin }) {
+
+    //imposto valori di default e check di default
+    const [data, setData] = useState({
+        username: '',
+        password: '',
+        remember: false,
+    })
+
+    //funzione per gestire eventi negli input
+    function handleInputs(event) {
+        //variabili di target
+        const name = event.target.name;
+        const value = event.target.value;
+
+        const checked = event.target.checked
+        const type = event.target.type
+        //al succedere dell evento restituisce tupla con valori inseriti negli input
+        setData((d) => {
+            return {
+                ...d,
+                [name]: type === 'checkbox' ? checked : value,
+            }
+        })
+    }
+
+    //funzione per stampare state aggiornato
+    function onLogin(e) {
+        //previene uso di default del form
+        e.preventDefault();
+        //stampa in console nuovo data in formato json
+        console.log(JSON.stringify(data, null, 2))
+    }
+
+    //funzione per resettare input
+    function handleResetInput() {
+        setData({
+            username: '',
+            password: '',
+            remember: '',
+        })
+    }
+
+    return (
+        <div>
+            <form name="form" type="submit" onSubmit={onLogin}>
+                <input name="username" type="text" value={data.username} onChange={handleInputs} placeholder="username" />
+                <input name="password" type="password" value={data.password} onChange={handleInputs} placeholder="password" />
+                <input name="remember" type="checkbox" checked={data.remember} onChange={handleInputs} />
+                <button disabled={!data.username || !data.password} /* onClick={onLogin} */>Login</button>
+                <button onClick={handleResetInput}>Reset</button>
+            </form>
+        </div>
+    )
+}
